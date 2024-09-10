55fe4817deccaa105d46ebf71b6516cc5c3fea89
GHOST <ghostdevbusiness@gmail.com>
2024-09-10 18:03:28 +0100

    karma

--- src/utils/versioning.ts
+++ src/utils/versioning.ts
@@ -2,6 +2,9 @@ import semverValid from 'semver/functions/valid'
 import semverGt from 'semver/functions/gt'
 
 export function sanitizeVersion(version: string): string | undefined {
+  return version.includes('@karma/app@')
+    ? version.replace('@karma/app@', '')
+    : undefined
   // Works with or without v in version
   const semanticV = version.split('v').pop()
   return semanticV
